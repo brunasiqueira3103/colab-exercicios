882f658b90c42255d6588731b6604e1d9dad4281
Bruna Siqueira <104859742+brunasiqueira3103@users.noreply.github.com>
2022-08-28 15:25:44 -0300

    Create divisaoSimples2.py

---
+++ divisaoSimples2.py
@@ -0,0 +1,10 @@
+#o mercado público foi construído em uma área de 6000 metros quadrados na preparação do terreno o espaço 
+#foi dividido em três partes iguais duas duas partes foram utilizados para construir 50 boxes para os feirantes
+# e a parte que sobrou foi reservada ao estacionamento. calcule qual a área de boxe construído.​
+
+mercado = int(6000)
+boxes = mercado //3 * 2
+estacionamento = mercado // 3 * 1
+print("A área contruída destinada os boxes são de: ", boxes,"metros quadrados.")
+
+#saída: A área contruída destinada os boxes são de:  4000 metros quadrados.
